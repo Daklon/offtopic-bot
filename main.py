eae039de0aab2d3a357aa15f166fb07a558f9b08
Javier <muchorrollodejavi@gmail.com>
2019-12-13 20:07:57 +0000

    initial implementation for manage commands, improved a bit the code

--- main.py
+++ main.py
@@ -26,50 +26,66 @@ class OfftopicBotHandler(telepot.aio.helper.ChatHandler):
         content_type,_,_ = telepot.glance(msg)
         print(content_type)
         if content_type == "text":
-            # translate the received message to lower case
-            msg['text'] = msg['text'].lower()
-            # if the message contains any keyword
-            if any(word in msg['text'] for word in self.keywords) and not self.waiting_reponse:
-                self.user_request_id.append(msg['from']['id'])
-                self.waiting_reponse = True
-                await self.sender.sendMessage(self.cfg['messages']['request'],parse_mode="Markdown")
+            #if the message contains sudo its a management command
+            if('sudo' in msg['text']):
+                await self.management_processor(msg)
+            #process normal messages
+            else:
+                await self.generic_processor(msg)
 
-            elif msg['text'] in self.accept and self.waiting_reponse:
-                if msg['from']['id'] in self.user_request_id:
-                    await self.sender.sendMessage(self.cfg['messages']['deny_retry'].format(msg['from']['username']),parse_mode="Markdown")
-                else:
-                    await self.sender.sendMessage(self.cfg['messages']['accept'],parse_mode="Markdown")
-                    await self.sender.sendPhoto(self.request_image())
-                    self.waiting_reponse = False
-            
-            elif msg['text'] in self.deny and self.waiting_reponse:
-                await self.sender.sendMessage(self.cfg['messages']['deny'],parse_mode="Markdown")
-                self.waiting_reponse = False
         # This will detect when the bot is added to a new group
         # and then he will introduce himself git the message 'group_intro'
         elif content_type == "new_chat_member":
             me = await self.bot.getMe()
             if msg['new_chat_participant']['id'] == me['id']:
                 await self.sender.sendMessage(self.cfg['messages']['group_intro'],parse_mode="Markdown")
-        # if the warmode is enabled and other bot send a photo, it will answer with two
-        elif (content_type == "photo" or content_type == "sticker") and self.warmode and msg['from']['is_bot']:
-            await self.sender.sendMessage(self.cfg['messages']['antibot'],parse_mode="Markdown")
-            await self.sender.sendPhoto(self.request_image())
-            await self.sender.sendPhoto(self.request_image())
-
+                
+    
+    # checks if ri returns an image, if not, then request another one until one is received
     def request_image(self):
         link = self.ri.get_image()
         while link is None:
             link = self.ri.get_image()
         return link
 
+    #commands that configure the bot
+    async def management_processor(self,msg):
+        #check if the user has privileges
+        if msg['from']['id'] in self.cfg['bot']['ownerids']:
+            pass #TODO
+        else:
+            #user has no privileges
+             await self.sender.sendSticker(self.cfg['messages']['notallowedurl'])
+    async def generic_processor(self,msg):
+        # translate the received message to lower case
+        msg['text'] = msg['text'].lower()
+        # if the message contains any keyword
+        if any(word in msg['text'] for word in self.keywords) and not self.waiting_reponse:
+            self.user_request_id.append(msg['from']['id'])
+            self.waiting_reponse = True
+            await self.sender.sendMessage(self.cfg['messages']['request'],parse_mode="Markdown")
+
+        elif msg['text'] in self.accept and self.waiting_reponse:
+            if msg['from']['id'] in self.user_request_id:
+                await self.sender.sendMessage(self.cfg['messages']['deny_retry'].format(msg['from']['username']),parse_mode="Markdown")
+            else:
+                await self.sender.sendMessage(self.cfg['messages']['accept'],parse_mode="Markdown")
+                await self.sender.sendPhoto(self.request_image())
+                self.waiting_reponse = False
+            
+        elif msg['text'] in self.deny and self.waiting_reponse:
+            await self.sender.sendMessage(self.cfg['messages']['deny'],parse_mode="Markdown")
+            self.waiting_reponse = False
+        
 
 #Initialization
 with open(sys.argv[1]+"/config.yaml", 'r') as yamlfile:
     cfg = yaml.safe_load(yamlfile)
 
-TOKEN = cfg['bot']['token']
+TOKEN = cfg['bot']['token'] #read the telegram bot token from the config file
 
+# create a handler for each telegram chat with a lifespan of "timeout"
+# if timeout is reached without any new messages it will be destroyed
 bot = telepot.aio.DelegatorBot(TOKEN, [
     pave_event_space()(
         per_chat_id(), create_open, OfftopicBotHandler, timeout=cfg['bot']['timeout']),
